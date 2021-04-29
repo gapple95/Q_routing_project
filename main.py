b2b42da942028bfb7670ac02768d0f96f3a9d151
Gapple <gapple95@naver.com>
2021-04-29 12:01:11 +0900

    shortest_path_routing 최종 테스트

--- main.py
+++ main.py
@@ -1,25 +1,48 @@
 # import node
-import shorteset_path_routing as node
+import shortest_path_routing as node
 
 Nodes = list()
+avr = list()
 for i in range(0, 36):
     Nodes.append(node.Node(0, 0, i))
+    avr.append(0)
 
 for i in range(0, 36):
+    # print(i)
     Nodes[i].init_routing()
+    # print()
+
+# Nodes[35].init_routing()
 
 # for t in range(0, 36):
 #     for i in range(0, 36):
 #         # print("t : " + str(t) + ", i : " + str(i))
 #         Nodes[i].activate(t)
+avr_count = 100
+for a in range(0, avr_count):
+    for i in range(0, 36):
+        for j in range(0, 36):
+            if i == j:
+                continue
+            # print(str(i) + ", " + str(j))
+            Nodes[i].create_packet(0, j)
+            for l in range(0, 12):
+                for k in range(0, 36):
+                    Nodes[k].activate(0)
+
+    for i in range(0, 36):
+        avr[i] = avr[i] + Nodes[i].hop_count
+
+    for i in range(0, 36):
+        Nodes[i].init_routing()
 
 for i in range(0, 36):
-    for j in range(0, 36):
-        if i == j:
-            continue
-        Nodes[i].create_packet(0, j)
-        for k in range(0, 36):
-            Nodes[k].activate(0)
+    if i % 6 == 0:
+        print()
+    print(str(avr[i] / avr_count) + " ", end='')
+print()
+
+# Nodes[35].create_packet(0, 28)
 
 # for i in range(0, 36):
 #     Nodes[i].create_packet(0, i)
@@ -28,19 +51,12 @@ for i in range(0, 36):
 #     for i in range(0, 36):
 #         Nodes[i].activate(t)
 #
-for i in range(0, 36):
-    if i % 6 == 0:
-        print()
-    print(str(len(Nodes[i].success)) + " ", end='')
-
-
-print()
 
 for i in range(0, 36):
     if i % 6 == 0:
         print()
     print(str(Nodes[i].hop_count) + " ", end='')
+print()
 
-# print()
 # for i in range(0, len(Nodes[34].success)):
 #     print(str(Nodes[34].success[i].source) + " ", end='')
